3435937de65c955193f098f55299340720064c12
human <bondarenkoola228@gmail.com>
2026-03-26 14:50:12 +0500

    Создание формы FormBooks

---
+++ ReadCity/FormBooks.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ReadCity
+{
+    partial class FormBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            panelTop = new Panel();
+            lblUserName = new Label();
+            btnLogut = new Button();
+            dgvBooks = new DataGridView();
+            panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvBooks).BeginInit();
+            SuspendLayout();
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(lblUserName);
+            panelTop.Controls.Add(btnLogut);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(10, 10);
+            panelTop.Name = "panelTop";
+            panelTop.Size = new Size(964, 40);
+            panelTop.TabIndex = 0;
+            // 
+            // lblUserName
+            // 
+            lblUserName.AutoSize = true;
+            lblUserName.Dock = DockStyle.Right;
+            lblUserName.Location = new Point(769, 0);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(45, 19);
+            lblUserName.TabIndex = 6;
+            lblUserName.Text = "label1";
+            lblUserName.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // btnLogut
+            // 
+            btnLogut.BackColor = Color.FromArgb(74, 111, 165);
+            btnLogut.BackgroundImageLayout = ImageLayout.Center;
+            btnLogut.Dock = DockStyle.Right;
+            btnLogut.FlatAppearance.BorderSize = 0;
+            btnLogut.FlatStyle = FlatStyle.Flat;
+            btnLogut.Location = new Point(814, 0);
+            btnLogut.Name = "btnLogut";
+            btnLogut.Size = new Size(150, 40);
+            btnLogut.TabIndex = 5;
+            btnLogut.TabStop = false;
+            btnLogut.Text = "Выйти";
+            btnLogut.UseVisualStyleBackColor = false;
+            // 
+            // dgvBooks
+            // 
+            dgvBooks.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvBooks.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvBooks.BackgroundColor = Color.White;
+            dgvBooks.BorderStyle = BorderStyle.None;
+            dgvBooks.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvBooks.ColumnHeadersVisible = false;
+            dgvBooks.Dock = DockStyle.Fill;
+            dgvBooks.Location = new Point(10, 50);
+            dgvBooks.MultiSelect = false;
+            dgvBooks.Name = "dgvBooks";
+            dgvBooks.ReadOnly = true;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            dgvBooks.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgvBooks.RowHeadersVisible = false;
+            dgvBooks.SelectionMode = DataGridViewSelectionMode.FullColumnSelect;
+            dgvBooks.Size = new Size(964, 601);
+            dgvBooks.TabIndex = 1;
+            // 
+            // FormBooks
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(984, 661);
+            Controls.Add(dgvBooks);
+            Controls.Add(panelTop);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(4);
+            Name = "FormBooks";
+            Padding = new Padding(10);
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Список книг";
+            panelTop.ResumeLayout(false);
+            panelTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvBooks).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelTop;
+        private Label lblUserName;
+        private Button btnLogut;
+        private DataGridView dgvBooks;
+    }
+}
---
+++ ReadCity/FormBooks.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReadCity
+{
+    public partial class FormBooks : Form
+    {
+        public User CurrentUser { get; private set; }
+        public bool IsGuest { get; private set; }
+        public FormBooks(User user, bool guest)
+        {
+            InitializeComponent();
+        }
+    }
+}
